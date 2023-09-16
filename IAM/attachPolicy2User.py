d391d0fde310853d1678effc36c01f776ecca7a9
iamigui <miguiorotor@gmail.com>
2023-09-16 20:48:43 +0200

    Updates IAM

--- IAM/attachPolicy2User.py
+++ IAM/attachPolicy2User.py
@@ -1,49 +1,113 @@
 import boto3
+from colorama import init, Fore, Back, Style
+import sys
 
-print('This is a function to attach policies to users.\n')
-print('Tell me the name of the user:')
-username = input(int())
+init(autoreset=True)
+
+def checkUser():
+    try:
+        print('Insert the name of the user: ')
+        iam = boto3.client('iam')
+        username = input()
+        username_encontrada = False
+        paginator = iam.get_paginator('list_users')
+
+        for response in paginator.paginate():
+            for user in response['Users']:
+                if username == user['UserName']:
+                    arn = user['Arn']
+                    print(Fore.GREEN + '\nThe username: ' + username + ' exists.')
+                    username_encontrada = True
+                    break
+
+        if not username_encontrada:
+            print(Fore.RED + '\nUnknown user.')
+            sys.exit(1)
+    
+    except Exception as e:
+        print('An error occurred:', e)
 
 def listPolicies():
-    iam = boto3.client('iam')
-    paginator = iam.get_paginator('list_policies')
-    print('Para esto necesitas saber el ARN de la policy que buscas...')
-    print('¿Sabes el nombre de la policy que buscas?: y/n ')
-    respuesta = input()
-    if respuesta == 'y':
-        print('Dime el nombre de la policy: ')
-        policy_name = input()
-        for response in paginator.paginate(Scope='AWS'):
-            for policy in response['Policies']:
-                if policy_name in policy['PolicyName']:
-                    Arn = policy['Arn']
-                    print('Policy Name : {} Arn : {}'.format(policy_name, Arn))
-                    break
-                else:
-                    print('Nombre de policy desconocido.')
-                    break
-            break
+    try:
+        iam = boto3.client('iam')
+        while True:
+            policy_encontrada = False
+            paginator = iam.get_paginator('list_policies')
+            print('I need to know the ARN from the policy you look for...')
+            print('Select how you want to display the policies:\n')
+            print('1) Verify the policy ARN: ')
+            print('2) List all policies: ')
+            print('3) Check the policies that contain the text: \n')
 
-    elif respuesta == 'n' :
-        print('This is the list of policies from AWS:')
-        for response in paginator.paginate(Scope="AWS"): #The scope can be changed to the customer, Local or AWS to list all policies
-            for policy in response['Policies']:
-                policy_name = policy['PolicyName']
-                Arn = policy['Arn']
+            respuesta = input()
+            
+            if respuesta == '1':
+                print('Tell me the name of policy: ')
+                global policy_name
+                policy_name = input(str())
+                for response in paginator.paginate(Scope='All'):
+                    for policy in response['Policies']:
+                        if policy_name == policy['PolicyName']:
+                            Arn = policy['Arn']
+                            print('Policy Name : {} Arn : {}'.format(policy_name, Arn))
+                            policy_encontrada = True
+                            break
 
-                print('Policy Name : {} Arn : {}'.format(policy_name, Arn))
+                if not policy_encontrada:
+                    print('Unknown policy name.\n')
 
-listPolicies()
+            elif respuesta == '2' :
+                print('This is the list of policies from AWS:')
+                for response in paginator.paginate(Scope="AWS"): #The scope can be changed to the customer, Local or AWS to list all policies
+                    for policy in response['Policies']:
+                        policy_name = policy['PolicyName']
+                        Arn = policy['Arn']
+
+                        print('Policy Name : {} Arn : {}'.format(policy_name, Arn))
+
+            elif respuesta == '3':
+                print('Tell me the name of policy: ')
+                policy_name = input(str())
+                for response in paginator.paginate(Scope='All'):
+                    for policy in response['Policies']:
+                        if policy_name in policy['PolicyName']:
+                            Arn = policy['Arn']
+                            print('Policy Name : {} Arn : {}'.format(policy['PolicyName'], Arn))
+                            policy_encontrada = True
+                            break
+
+                if not policy_encontrada:
+                    print('Unknown policy name.')
+
+            else:
+                print('\nNot valid option.')
+
+    except Exception as e:
+        print('An error occurred:', e)
+        sys.exit(1)
 
 def attachPolicy(policy_arn, username):
-    iam = boto3.client('iam')
+    try:
+        iam = boto3.client('iam')
 
-    input('Dime el policy ARN: ')
-    response = iam.attach_user_policy(
-        UserName = username,
-        PolicyArn = policy_arn
-    )
+        response = iam.attach_user_policy(
+            UserName = username,
+            PolicyArn = policy_arn
+        )
 
-    print('The policy ' + policy_name + ' has been attached to the user: ' + username)
+        print('The policy ' + policy_name + ' has been attached to the user: ' + username)
+    
+    except Exception as e:
+        print('An error occurred:', e)
+        sys.exit(1)
 
-attachPolicy(policy_arn, username)
+if __name__ == "__main__":
+    print('This is a function to attach policies to users.\n')
+    checkUser()
+
+    listPolicies()
+
+    print('Insert the policy ARN: ')
+    policy_arn = input()
+
+    attachPolicy(policy_arn, username)
--- IAM/listUsers.py
+++ IAM/listUsers.py
@@ -1,13 +1,59 @@
 import boto3
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
 
 def allUsers():
     iam = boto3.client('iam')
     paginator = iam.get_paginator('list_users')
 
-    for response in paginator.paginate():
-        for user in response['Users']:
-            username = user['UserName']
-            arn = user['Arn']
-            print('The username: ' + username + ' has the arn ' + arn)
+    print('Select how you want to display the users:\n')
+    print('1) List all users: ')
+    print('2) Verify if an user exists: ')
+    print('3) Check user ARN: \n')
+
+    eleccion = input()
+
+    if eleccion == '1':
+        print('You chose to list all users:\n')
+        for response in paginator.paginate():
+            for user in response['Users']:
+                username = user['UserName']
+                arn = user['Arn']
+                print(Fore.GREEN + '\nThe username: ' + username + ' has the arn ' + arn)
+    
+    elif eleccion == '2':
+        print('\nInsert the username: ')
+        username = input()
+
+        username_encontrada = False
+
+        for response in paginator.paginate():
+            for user in response['Users']:
+                if username == user['UserName']:
+                    arn = user['Arn']
+                    print(Fore.GREEN + '\nThe username: ' + username + ' exists.')
+                    username_encontrada = True
+                    break
+
+        if not username_encontrada:
+            print(Fore.RED + '\nUnknown user.')
+    
+    elif eleccion == '3':
+        print('\nInsert the username: ')
+        username = input()
+
+        username_encontrada = False
+
+        for response in paginator.paginate():
+            for user in response['Users']:
+                if username == user['UserName']:
+                    arn = user['Arn']
+                    print(Fore.GREEN + '\nThe username: ' + username + ' has the arn ' + arn)
+                    username_encontrada = True
+                    break
+
+        if not username_encontrada:
+            print(Fore.RED + '\nUnknown user.')
 
 allUsers()
